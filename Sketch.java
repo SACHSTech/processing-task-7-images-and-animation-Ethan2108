bd79563b95f60b1c8d7c30d65ae4408adfc61d88
Ethan2108 <ethan.au23@ycdsbk12.ca>
2022-04-19 13:36:11 -0400

    added images, some code

--- Sketch.java
+++ Sketch.java
@@ -1,13 +1,17 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Sketch extends PApplet {
 	
+  PImage imgFishy;
+  PImage imgSharky;
+  PImage imgBackground;
+  //
 	
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
-	// put your size call here
     size(400, 400);
   }
 
@@ -16,14 +20,22 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    imgFishy = loadImage("fish-png"); // load the image into the program
+    //resize fish
+    imgFishy.resize(imgFishy.width/4,imgFishy.height/4);
+
+    // load shark
+    imgSharky = loadImage("shark.png");
+
+    imgBackground = loadImage("Beach.jpeg")                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              .jpeg");
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
+	  background(0, 255, 238);
+    image(imgBackground, 255, 238);
   
   // define other methods down here.
 }}
